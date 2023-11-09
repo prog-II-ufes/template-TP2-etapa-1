427fb757e8f9df21b2a3504d5a5980bc89ebce77
paaatcha <pacheco.comp@gmail.com>
2023-11-09 15:40:54 -0300

    melhorando a descricao dos docstrings

--- Includes/tDocumento.h
+++ Includes/tDocumento.h
@@ -4,6 +4,11 @@
 #define _TDOCUMENTO_H_
 
 typedef struct tDocumento tDocumento;
+
+/**
+ * Funções de callback que serão utilizadas para imprimir o documento na tela,
+ * em arquivo e desalocar o dado genérico, respectivamente.
+ */
 typedef void (*func_ptr_imprimeNaTela)(void *dado);
 typedef void (*func_ptr_imprimeEmArquivo)(void *dado, char *path);
 typedef void (*func_ptr_desaloca)(void *dado);
@@ -19,18 +24,18 @@ tDocumento *criaDocumento(void *dado, func_ptr_imprimeNaTela imprimeNaTela,
 /**
  * Função que recebe um Documento e o desaloca. O dado genérico também deve ser desalocado
  * dentro dessa função.
- */                        
+ */
 void desalocaDocumento(tDocumento *doc);
 
 /**
  * Função que recebe um Documento genérico e o imprime na tela.
-*/
+ */
 void imprimeNaTelaDocumento(tDocumento *doc);
 
 /**
  * Função que recebe um Documento genérico e o imprime em um arquivo.
  * O path representa o caminho até o diretório onde o arquivo será criado ou editado.
-*/
+ */
 void imprimeEmArquivoDocumento(tDocumento *doc, char *path);
 
 #endif
--- Includes/tFila.h
+++ Includes/tFila.h
@@ -15,11 +15,12 @@ tFila *criaFila();
 /**
  * Função que recebe um ponteiro para uma fila de impressao e desaloca toda a memória
  * associada a ela. Essa função também desaloca todos os documentos associados a fila.
+ * Essa função sempre verifica se o ponteiro é NULL antes de desalocar.
  */
 void desalocaFila(tFila *f);
 
 /**
- * Função que recebe uma fila, um dado genérico e suas funções de callback para criar um Documento
+ * Função que recebe uma fila, um dado genérico e suas funções de callback para criar um Documento com esses elementos
  * e inseri-lo na fila de impressão. Perceba que a fila é agnóstica ao tipo de dado e pode conter
  * múltiplos Documentos (limitados pela memória) aguardando para serem impressos.
  *
@@ -34,9 +35,9 @@ void insereDocumentoFila(tFila *f, void *dado, func_ptr_imprimeNaTela imprimeNaT
 int quantidadeDocumentosNaFila(tFila *f);
 
 /**
- * Função que executa a impressão de todos os documentos da fila.
- * A impressão é feita em duas etapas. Primeiro Documento é impresso na tela e depois em arquivo.
- * A função também recebe o path para o diretóio onde o arquivo deve ser criado ou editado
+ * Função que invoca as funções de impressão de Documento executando a impressão de todos os documentos da fila.
+ * A impressão é feita em duas etapas, primeiro o Documento é impresso na tela e depois em arquivo.
+ * A função também recebe o path para o diretório onde o arquivo deve ser criado ou editado
  */
 void imprimeFila(tFila *f, char *path);
 
